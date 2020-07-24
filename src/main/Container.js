02a0b5eed469cc244c79132628e49c9d4cc62b86
Altaf <samaltaf6@gmail.com>
2020-07-24 10:34:17 +0530

    initial cleanup

---
+++ src/main/Container.js
@@ -0,0 +1,12 @@
+import React, { Fragment } from 'react';
+import Main from './Main';
+
+const Container = () => {
+  return (
+    <Fragment>
+      <Main></Main>
+    </Fragment>
+  );
+};
+
+export default Container;
